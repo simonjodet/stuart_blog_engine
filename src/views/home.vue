d421b778fcf0c7dd7a7a51e20b358cbf947e41cd
Simon Jodet <simon@jodet.com>
2019-04-22 10:04:47 +0200

    Load post list

---
+++ src/views/home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <pre>{{ JSON.stringify(posts, null, 2) }}</pre>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+//import HelloWorld from "@/components/HelloWorld.vue";
+import axios from "axios";
+
+export default {
+  name: "home",
+
+  data: () => {
+    return { posts: null };
+  },
+
+  created() {
+    axios({ method: "get", url: "/posts.json" }).then(({ data: posts }) => {
+      this.posts = new Map(JSON.parse(JSON.stringify(posts)));
+    });
+  }
+};
+</script>

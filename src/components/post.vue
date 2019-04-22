f4a10c7cce49c32033815d13563cb04bdadd8737
Simon Jodet <simon@jodet.com>
2019-04-22 17:20:11 +0200

    Load posts and display them

---
+++ src/components/post.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div v-if="post.html" v-html="post.html"></div>
+    <v-icon v-else>mdi-spin mdi-loading</v-icon>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "post",
+
+  props: {
+    post: {
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,26 +1,43 @@
 <template>
   <div>
-    <v-icon v-if="!posts">mdi-spin mdi-loading</v-icon>
-    <pre v-else>{{ JSON.stringify(posts, null, 2) }}</pre>
+    <div v-if="posts">
+      <post v-for="post in posts" :key="post.id" :post="post"></post>
+    </div>
+    <v-icon v-else>mdi-spin mdi-loading</v-icon>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-//import HelloWorld from "@/components/HelloWorld.vue";
+import Post from "@/components/post.vue";
 import axios from "axios";
+import marked from "marked";
 
 export default {
   name: "home",
+  components: { Post },
 
   data: () => {
     return { posts: null };
   },
 
   mounted() {
-    axios({ method: "get", url: "/posts.json" }).then(({ data: posts }) => {
-      this.posts = new Map(JSON.parse(JSON.stringify(posts)));
-    });
+    axios({ method: "get", url: "/posts.json" })
+      .then(({ data: posts }) => {
+        this.posts = posts;
+      })
+      .then(() => {
+        for (const [index, post] of this.posts.entries()) {
+          this._loadPost(post, index);
+        }
+      });
+  },
+
+  methods: {
+    _loadPost(post, index) {
+      axios({ method: "get", url: `/posts/${post.id}.md` }).then(({ data: markdown }) => {
+        this.$set(this.posts, index, { ...post, markdown, html: marked(markdown) });
+      });
+    }
   }
 };
 </script>
